764482be56bbb101bb54dca571ab9d8ec35e44d2
Larry Shannon <larry@breakawaydesign.com>
2024-04-08 05:16:57 -0700

    added delete account

--- SwipeAction/SettingsView.swift
+++ SwipeAction/SettingsView.swift
@@ -11,6 +11,7 @@ import FirebaseAuth
 struct SettingsView: View {
     @EnvironmentObject var userAuth: Authentication
     @State var showSignOut = false
+    @State var showDeleteAccount = false
     @State var showProfileSheet = false
     
     var body: some View {
@@ -31,7 +32,7 @@ struct SettingsView: View {
                 .buttonStyle(PlainTextButtonStyle())
                 .disabled(!Auth.auth().userIsLoggedIn)
                 .alert("Sign Out?", isPresented: $showSignOut) {
-                    Button("Cancel", role: .cancel) { /* showSignOut = false */ }
+                    Button("Cancel", role: .cancel) { }
                     Button("Sign Out", role: .destructive) {
                         do {
                             try Auth.auth().signOut()
@@ -41,6 +42,29 @@ struct SettingsView: View {
                     }
                 } message: {
                     Text("Are you sure you want to sign out of your account?")
+                }
+                Button("Delete Account") {
+                    showDeleteAccount = true
+                }
+                .buttonStyle(PlainTextButtonStyle())
+                .alert("Delete Account?", isPresented: $showDeleteAccount) {
+                    Button("Cancel", role: .cancel) {  }
+                    Button("Delete", role: .destructive) {
+                        Task {
+                            guard let user = Auth.auth().currentUser else {
+                                debugPrint(String.boom, "Delete User could not auth current user")
+                                return
+                            }
+                            do {
+                                try await user.delete()
+                            } catch {
+                                debugPrint(String.boom, "Delete User could not delete user: \(error)")
+                                try? Auth.auth().signOut()
+                            }
+                        }
+                    }
+                } message: {
+                    Text("Are you sure you want to delete your account?")
                 }
                 Spacer()
             }
